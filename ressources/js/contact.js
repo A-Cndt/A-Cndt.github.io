0df507b65a472cf779865bc5007bd6377f9919fd
ACE <alexandre.condette@wanadoo.fr>
2023-08-02 21:11:19 +0200

    Formulaire de contact

---
+++ ressources/js/contact.js
@@ -0,0 +1,61 @@
+<script src="https://smtpjs.com/v3/smtp.js"></script>
+
+function validateForm()                                    
+{ 
+    var name = document.forms["formContact"]["name"];               
+    var email = document.forms["formContact"]["email"];    
+    var message = document.forms["formContact"]["message"];   
+   
+    if (name.value == "")                                  
+    { 
+        document.getElementById('errorname').innerHTML="Veuillez entrez un nom valide";  
+        name.focus(); 
+        return false; 
+    }else{
+        document.getElementById('errorname').innerHTML="";  
+    }
+       
+    if (email.value == "")                                   
+    { 
+        document.getElementById('erroremail').innerHTML="Veuillez entrez une adresse mail valide"; 
+        email.focus(); 
+        return false; 
+    }else{
+        document.getElementById('erroremail').innerHTML="";  
+    }
+   
+    if (email.value.indexOf("@", 0) < 0)                 
+    { 
+        document.getElementById('erroremail').innerHTML="Veuillez entrez une adresse mail valide"; 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf(".", 0) < 0)                 
+    { 
+        document.getElementById('erroremail').innerHTML="Veuillez entrez une adresse mail valide"; 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (message.value == "")                           
+    {
+        document.getElementById('errormsg').innerHTML="Veuillez entrez un message valide"; 
+        message.focus(); 
+        return false; 
+    }else{
+        document.getElementById('errormsg').innerHTML="";
+        
+        Email.send({
+            To : 'alexandre.condette@spacebel.fr',
+            From : email.value,
+            Subject : "Formulaire de contact:",
+            Body : message.value,
+        }).then(
+          message => alert(message)
+        );
+
+    }
+   
+    return true; 
+}
